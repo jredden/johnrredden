d7cad0265fbd89b276b3d0eb122e6f4ff8232fc4
jredden <jredden@jredden-laptop>
2014-12-22 20:10:26 -1000

    cluster w.i.p.

---
+++ src/main/java/com/zenred/cosmos/domain/ClusterRepDao.java
@@ -0,0 +1,12 @@
+package com.zenred.cosmos.domain;
+
+import com.zenred.johntredden.domain.AbstractJDBCDao;
+
+public class ClusterRepDao extends AbstractJDBCDao{
+	
+	public static String CLUSTER_REP = "ClusterRep";
+	public static String CLUSTER_REP_ID = "clusterRepID";
+	public static String SYSTEM_ID = "systemId";
+	public static String CLUSTER_NAME = "clusterName";
+
+}
