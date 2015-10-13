7aa7ebf85e1130354f1c3f80f3762c6a6a406b42
jredden <johnredden@aol.com>
2015-10-12 21:11:32 -1000

    post processing atmosphere w.i.p. 0

--- src/main/java/com/zenred/cosmos/service_rules_and_infrastructure/PostProcessAtmosphere.java
+++ src/main/java/com/zenred/cosmos/service_rules_and_infrastructure/PostProcessAtmosphere.java
@@ -1,5 +1,10 @@
 package com.zenred.cosmos.service_rules_and_infrastructure;
 
+import org.kie.api.KieServices;
+import org.kie.api.event.rule.DebugAgendaEventListener;
+import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 
 public class PostProcessAtmosphere {
