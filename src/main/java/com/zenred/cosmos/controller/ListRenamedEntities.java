4b088539a47cd7aca196f6c713b92ed54d905225
jredden <johnredden@aol.com>
2018-12-25 18:26:14 -1000

    rename a astro entity 41

--- src/main/java/com/zenred/cosmos/controller/ListRenamedEntities.java
+++ src/main/java/com/zenred/cosmos/controller/ListRenamedEntities.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+
+import com.zenred.cosmos.controller.json.RenameResponseView;
 import com.zenred.cosmos.domain.RenameObjectType;
 import com.zenred.cosmos.service_rules_and_infrastructure.NamedSystemDestinations;
 import com.zenred.cosmos.vizualization.GenericRenameResponseForDestintion;
@@ -21,8 +23,9 @@ public class ListRenamedEntities implements Controller {
 		String objectType = request.getParameter("objectType");
 		RenameObjectType renameObjectType = RenameObjectType.valueOf(request.getParameter(objectType));
 		List<GenericRenameResponseForDestintion> responseList = NamedSystemDestinations.returnSelections(renameObjectType);
-		
-		return null;
+		ModelAndView modelAndView = new ModelAndView(new RenameResponseView());
+		modelAndView.addObject(RenameResponseView.JSON_ROOT, responseList);
+		return modelAndView;
 	}
 
 }
